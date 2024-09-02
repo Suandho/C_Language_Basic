6fdd2d53329b3451dac1a74cd24864a53482fe02
SUJEONG <limct2230@gmail.com>
2024-09-03 00:00:13 +0900

    part 3 / part 4 문제풀이 손으로 푸는 문제들은 넘김

---
+++ C_Language_Basic/C_Language_Basic/part_3.cpp
@@ -0,0 +1,52 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	//part 3-1
+//	
+//	//Problem_1
+//	/*int num = 0;
+//	int num2 = 0;
+//
+//	printf("Number: ");
+//	scanf("%d  %d", &num, &num2);
+//
+//	printf("Plus: %d - %d = %d\n", num, num2, num - num2);
+//	printf("Multiply: %d X %d = %d\n", num, num2, num * num2);*/
+//
+//	//problem_2
+//	//int num, num2, num3 = 0;
+//	//printf("정수 입력 : ");
+//	//scanf_s("%d %d %d", &num, &num2, &num3);
+//
+//	//printf("%d X %d + %d = %d", num, num2, num3, num * num2 + num3);
+//	
+//	//problem_3
+//	/*int num = 0;
+//	printf("정수입력 : ");
+//	scanf_s("%d", &num);
+//
+//	printf("제곱 : %d \n ", num*num);*/
+//
+//	//problem_4
+//	/*int num, num2 = 0;
+//	printf("정수 두개 입력 :");
+//	scanf_s("%d  %d", &num, &num2);
+//
+//	printf("몫 ; %d \n", num / num2);
+//	printf("나머지 %d\n", num % num2);*/
+//
+//	/*int num1, num2, num3 = 0;
+//	int plus = 0;
+//	int minus = 0;
+//	int rest = 0;
+//	printf("정수입력 : ");
+//	scanf_s("%d %d %d", &num1, &num2, &num3);
+//
+//	minus = num1 - num2;
+//	plus = num2 + num3;
+//	rest = num3 % num1;
+//	printf("%d X %d X %d = %d", minus, plus, rest, minus * plus * rest);*/
+//
+//	return 0;
+//}
